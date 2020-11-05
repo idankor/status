2b13b6b21f488ce26a30a0c87ffd27c6d625b140
idankor <idankor@gmail.com>
2020-11-05 20:30:48 +0200

    2

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 let theGender = undefined;
+let stepNumber = 1;
 
-// STEP 1
+// STEP 1 - GENDER
 
-document.getElementById('btn-male').addEventListener('click', (event) => {
-    document.getElementById('the-output').value += '\r\n\r\n';
-    document.getElementById('the-output').value += ' בן';
-    theGender = 'male';
+if (stepNumber == 1) {
+  document.getElementById("btn-male").addEventListener("click", (event) => {
+    document.getElementById("the-output").value += "\r\n\r\n";
+    document.getElementById("the-output").value += " בן";
+    theGender = "male";
     clearCard();
-    let tempElement = document.createElement('div');
+    let tempElement = document.createElement("div");
     tempElement.innerHTML = "גיל";
-    tempElement.classList.add('current-title');
-    document.getElementById('main-card').appendChild(tempElement);
-    tempElement = document.createElement('input');
-    tempElement.classList.add('field-lg');
-    tempElement.id = 'current-field';
-    document.getElementById('main-card').style.flexDirection = 'column';
-    document.getElementById('main-card').style.justifyContent = 'space-between';
-    document.getElementById('main-card').style.padding = '20% 0px 20% 0px';
-    document.getElementById('main-card').appendChild(tempElement);
-    document.getElementById('current-field').focus();
-    document.addEventListener("keyup", event => {
-        if (event.keyCode == 13) {
-            console.log('You pressed enter.');
-        }
-    })
-});
+    tempElement.classList.add("current-title");
+    document.getElementById("main-card").appendChild(tempElement);
+    tempElement = document.createElement("input");
+    tempElement.classList.add("field-lg");
+    tempElement.id = "current-field";
+    document.getElementById("main-card").style.flexDirection = "column";
+    document.getElementById("main-card").style.justifyContent = "space-between";
+    document.getElementById("main-card").style.padding = "20% 0px 20% 0px";
+    document.getElementById("main-card").appendChild(tempElement);
+    document.getElementById("current-field").focus();
+    document.addEventListener("keyup", (event) => {
+      if (event.keyCode == 13) {
+        console.log("You pressed enter.");
+      }
+    });
+    stepNumber = 2;
+  });
 
-document.getElementById('btn-fe male').addEventListener('click', event => {
-    document.getElementById('the-output').value += '\r\n\r\n';
-    document.getElementById('the-output').value += ' בת';
-});
-
-function clearCard() {
-    document.getElementById('main-card').textContent = '';
+  document.getElementById("btn-fe male").addEventListener("click", (event) => {
+    document.getElementById("the-output").value += "\r\n\r\n";
+    document.getElementById("the-output").value += " בת";
+  });
 }
 
+function clearCard() {
+  document.getElementById("main-card").textContent = "";
+}
